5afa9147dce60a04a0c8a8aa130551b7a9effd03
Jason Schlesinger <schlesinger@plbl.net>
2020-07-03 14:11:12 -0700

    Several Bugfixes - Works in Test

--- src/lastlink/handler.py
+++ src/lastlink/handler.py
@@ -1,46 +1,53 @@
 # import requests
 import logging
-import boto3
 import json
+import os
+import boto3
+from boto3.dynamodb.conditions import Key
 from urllib.parse import parse_qs
 
 logger = logging.getLogger()
-# logger.setLevel(logging.INFO)
+if os.environ.get('Environment', 'Test') == 'Prod':
+    logger.setLevel(logging.INFO)
 logger.setLevel(logging.DEBUG)
 
 ddb = boto3.resource('dynamodb')
+table = ddb.Table('LastLinkTable')
 
 
-def params_from_headers(headers, body):
+def params_from_headers(headers, body, queryString):
     # Handles content both as JSON and WWW form
+    logger.debug(headers)
+    logger.debug(body)
     cf = "Content-Type"
     if cf not in headers:
+        parameters = {}
         cf = cf.lower()
-        if cf not in headers:
-            logger.error("Invalid Event: Paylad has not Content-Type header")
+        if cf not in headers and queryString is None:
+            logger.error("Invalid Event: Payload has no Content-Type header")
             raise Exception("Content-Type not found")
-    if headers[cf].startswith("application/x-www-form-urlencoded"):
-        parameters = parse_qs(body)
-        parameters = dict([(x, y[0]) for x, y in parameters.items()])
-    elif headers[cf].startswith("application/json"):
-        parameters = json.loads(body)
     else:
-        logger.error("Content is of type {} and not supported".format(headers[cf]))
-        raise Exception("Unsupported Content Type")
+        if headers[cf].startswith("application/x-www-form-urlencoded"):
+            parameters = parse_qs(body)
+            parameters = dict([(x, y[0]) for x, y in parameters.items()])
+        elif headers[cf].startswith("application/json"):
+            parameters = json.loads(body)
+        else:
+            logger.error("Content is of type {} and not supported".format(headers[cf]))
+            raise Exception("Unsupported Content Type")
+    parameters.update(queryString)
     return parameters
 
 
 def get_latest_link(linkId):
-    latest_link = ddb.query(
-        TableName='LastLinkTable',
-        KeyConditions={"LinkId": {
-                        "ComparisonOperator": "EQ",
-                        "AttributeValueList": [{"S": linkId}]}},
+    latest_link = table.query(
+        KeyConditionExpression=Key("LinkId").eq(linkId),
         ScanIndexForward=True,
         Limit=1)
-    if latest_link == {}:
+    logger.debug(latest_link)
+    if len(latest_link.get('Items', [])) == 0:
         raise Exception("Link ID %s Not Found" % linkId)
-    return latest_link
+    return latest_link['Items'][0]
 
 
 def set_latest_link(linkId, latest_link, newlink):
@@ -50,8 +57,8 @@ def set_latest_link(linkId, latest_link, newlink):
         'NodeIndex': latest_index,
         'Node': newlink
     }
-    table = ddb.table('LastLinkTable')
-    table.putItem(new_latest)
+    table.put_item(Item=new_latest)
+    return new_latest
 
 
 def lambda_handler(event, context):
@@ -60,7 +67,9 @@ def lambda_handler(event, context):
         # return server_error_reply("Invalid Resource")
         raise Exception("Invalid Resource")
     try:
-        parameters = params_from_headers(event["headers"], event.get("body", None))
+        parameters = params_from_headers(event["headers"],
+                                         event.get("body", None),
+                                         event.get('queryStringParameters',{}))
     except Exception as e:
         # return server_error_reply(e)
         raise Exception(e)
